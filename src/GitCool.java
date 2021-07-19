bf86be5687f82d3bb1a69db07c136c84dced35fd
B23-User <2302soboleva@gmail.com>
2021-07-19 15:49:03 +0000

    new

--- src/GitCool.java
+++ src/GitCool.java
@@ -1,5 +1,10 @@
 public class GitCool {
     public static void main(String[] args) {
-
+// we have addedour GitHub accaunt
+        // make sure to click applay and click on to save
+        // we were able to successefully shared project under our github accaunt
+        //it combines the 3 actions below
+        // create the link between ocal repo to remote repo
+        // push local commits to remote repo
     }
 }
